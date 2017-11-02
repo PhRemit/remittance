b24459cad5579aca666b9b4dd3f3868a4661b193
Ronald <phremit@hotmail.com>
2017-11-02 20:21:00 +1100

    Initial Dev Commot

---
+++ web/PhRemit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhRemit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
